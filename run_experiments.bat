626661a107d0227f7f753975d05f0e79460d85c4
Kevin Su <kevinsu997@gmail.com>
2020-02-10 19:54:08 +0100

    Cleaned up the Folder for Submission

--- run_experiments.bat
+++
@@ -1,1 +0,0 @@
-D:\Programming\NetLogo\netlogo-headless.bat --model compound-events-no-plots.nlogo --experiment base-experiment-trendtrue --table experiments\base-experiment-trendtrue.csv --threads 16
