e99e128cfc38472af78c96d8ee87127aefbd4a6c
Kevin Su <kevinsu997@gmail.com>
2020-02-10 19:57:41 +0100

    Added cleaned up files and zips

---
+++ experiments/run_experiments.bat
@@ -0,0 +1,1 @@
+D:\Programming\NetLogo\netlogo-headless.bat --model compound-events-no-plots.nlogo --experiment base-experiment-trendtrue --table experiments\base-experiment-trendtrue.csv --threads 16
